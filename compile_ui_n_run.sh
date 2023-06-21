a5494daea821196b6b40e5e5fb78dc831ce3df2e
Miguel Sequeira <miguel.sequeira@tecnico.ulisboa.pt>
2023-06-21 17:30:43 +0200

    Fixed the layout issues and changed the about menu.

--- compile_ui_n_run.sh
+++ compile_ui_n_run.sh
@@ -6,4 +6,13 @@ pyuic5 -o ndf_spectra_fit_ui.py ui/ndf_spectra_fit.ui;
 pyuic5 -o reactions_ui.py ui/reactions.ui; 
 pyuic5 -o NDF_run_ui.py ui/NDF_run.ui;
 pyuic5 -o ndf_more_options_ui.py ui/ndf_more_options.ui;
+
+# Replace paths in the generated Python files
+sed -i 's|ui/../logos/|logos/|g' main_window_ui.py
+sed -i 's|ui/../logos/|logos/|g' about_window_ui.py
+sed -i 's|ui/../logos/|logos/|g' ndf_spectra_fit_ui.py
+sed -i 's|ui/../logos/|logos/|g' reactions_ui.py
+sed -i 's|ui/../logos/|logos/|g' NDF_run_ui.py
+sed -i 's|ui/../logos/|logos/|g' ndf_more_options_ui.py
+
 python3 NDF_gui.py
